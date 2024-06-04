caadc08910d99d05eb30d56f0b8fcfc89d9f0b02
AshishAlbi <ashishalbi07@gmail.com>
2024-06-04 15:46:29 +0530

    update: config

--- copyFiles.js
+++ copyFiles.js
@@ -2,7 +2,7 @@ import { copy } from 'fs-extra';
 
 async function copyAssets() {
   try {
-    await copy('/assets', 'dist/assets');
+    await copy('assets', 'dist/assets');
     console.log('Assets copied successfully!');
   } catch (err) {
     console.error('Error copying assets:', err);
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { copy } from "vite-plugin-copy";
+// import { copy } from "vite-plugin-copy";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "/",
   plugins: [
-    react()],
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "assets/**/*",
+          dest: "assets",
+        },
+      ],
+    }),
+  ],
 });
