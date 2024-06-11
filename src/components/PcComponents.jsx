1a02af8acf0f4f8100f315547043d74dff2722b7
AshishAlbi <ashishalbi07@gmail.com>
2024-06-11 14:01:30 +0530

    update: mobile float check

--- src/components/PcComponents.jsx
+++ src/components/PcComponents.jsx
@@ -1,11 +1,20 @@
 import { useTheme } from "@mui/material/styles";
-import { Environment, Float, OrbitControls, useFBX, useGLTF } from "@react-three/drei";
+import {
+  Environment,
+  Float,
+  OrbitControls,
+  useFBX,
+  useGLTF,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { useEffect, useRef } from "react";
+import { isMobileDevice } from "../utils/isMobile";
 
 function PcComponents() {
   const theme = useTheme();
   const pc = useGLTF("/assets/models/mac-draco.glb");
+
+  const ismobile = isMobileDevice();
 
   return (
     <Canvas
@@ -16,13 +25,19 @@ function PcComponents() {
         attach="background"
         args={[theme.palette.mode == "light" ? "white" : "#000000"]}
       />
-      <Environment preset="city"/>
+      <Environment preset="city" />
       <directionalLight position={[0, 10, 45]} intensity={1} />
-      <Float floatIntensity={2} speed={2}>
-        <mesh scale={2} position={[0,-3,0]}>
+      {!ismobile ? (
+        <Float floatIntensity={2} speed={2}>
+          <mesh scale={2} position={[0, -3, 0]}>
+            <primitive object={pc.scene} />
+          </mesh>
+        </Float>
+      ) : (
+        <mesh scale={2} position={[0, -3, 0]}>
           <primitive object={pc.scene} />
         </mesh>
-      </Float>
+      )}
     </Canvas>
   );
 }
