aa2d938a1e3d67aa501de8af3845e95f42a7092a
AshishAlbi <ashishalbi07@gmail.com>
2024-09-02 13:13:31 +0530

    fix: commonised the canvas component

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-import PcComponents from "../components/PcComponents";
+import PcComponents from "../components/ThreeFibre/PcComponents";
 
 function AboutMe() {
   return (
