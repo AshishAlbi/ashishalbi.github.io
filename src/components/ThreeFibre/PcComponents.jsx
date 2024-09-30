bf22d6d2bd0bee4907b79c4feaa707437b66e76e
AshishAlbi <ashishalbi07@gmail.com>
2024-09-30 15:36:08 +0530

    Feat: added globe for mobile

--- src/components/ThreeFibre/PcComponents.jsx
+++ src/components/ThreeFibre/PcComponents.jsx
@@ -1,23 +1,14 @@
 import { useGLTF } from "@react-three/drei";
 import React from "react";
 import ThreeCanvas from "./ThreeCanvas";
-import { isMobileDevice } from "../../utils/isMobile";
 
 function PcComponents() {
-  const pc = useGLTF("/models/mac-draco.glb");
-  const isMobile = isMobileDevice();
-
+  const pc = useGLTF("/models/mac-draco.glb", { useDraco: true });
   return (
     <ThreeCanvas cameraPosition={[0, 10, 45]} isPcComponent={true}>
-      {isMobile ? (
-        <mesh scale={2} position={[0, -3, 0]}>
-          <primitive object={pc.scene} />
-        </mesh>
-      ) : (
-          <mesh scale={2} position={[0, -3, 0]}>
-            <primitive object={pc.scene} />
-          </mesh>
-      )}
+      <mesh scale={2} position={[0, -3, 0]}>
+        <primitive object={pc.scene} />
+      </mesh>
     </ThreeCanvas>
   );
 }
