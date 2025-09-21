65768a78f5c24937c77e4e33c4720c0f3fc2aa6e
AshishAlbi <ashishalbi07@gmail.com>
2025-09-22 01:30:01 +0530

    tailwind added

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-white w-full'>
+        <p className='font-dancing text-3xl'>Ashish.</p>
+    </div>
+  )
+}
+
+export default Navbar
